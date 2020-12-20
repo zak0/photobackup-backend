5bf21d084472cb5f09cddad16f2f2f35c1017253
jaakko <jaakko.jaaska@gmail.com>
2020-12-20 13:51:53 +0100

    Removes unused version name&code constants from Constants.kt

--- src/jaaska/jaakko/photosapp/server/Constants.kt
+++ src/jaaska/jaakko/photosapp/server/Constants.kt
@@ -1,8 +1,5 @@
 package jaaska.jaakko.photosapp.server
 
 object Constants {
-    val VERSION_CODE = 1
-    val VERSION_NAME = "0.1.0"
-
     val SUPPORTED_IMAGE_FILE_EXTENSIONS = listOf("png", "jpg")
 }
